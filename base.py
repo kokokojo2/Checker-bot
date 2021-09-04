5a733a8e6ffd4d76426ad29082039e72f4fff258
kokokojo2 <foxgunplay@gmail.com>
2021-09-04 10:44:53 +0300

    messages logic implemented

--- base.py
+++ base.py
@@ -6,34 +6,63 @@ class BaseFunctionSet:
     silence = {}
 
 
+class BaseMessenger:
+
+    def send_message(self, context):
+        raise NotImplementedError()
+
+    def send_success_message(self, context):
+        raise NotImplementedError()
+
+    def send_completed_message(self, context):
+        raise NotImplementedError()
+
+    def send_errors_message(self, context):
+        raise NotImplementedError()
+
+
 class Checker:
-    def __init__(self, function_set: BaseFunctionSet):
+    def __init__(self, function_set: BaseFunctionSet, messenger_obj: BaseMessenger):
         if not isinstance(function_set, BaseFunctionSet):
             raise ImproperlyConfigured(f'Checker`s __init__ method expects BaseFunctionSet instance, but'
                                        f' {type(function_set).__name__} was given instead.')
 
+        if not isinstance(messenger_obj, BaseMessenger):
+            raise ImproperlyConfigured(f'Checker`s __init__ method expects BaseMessenger instance, but'
+                                       f' {type(messenger_obj).__name__} was given instead.')
         raw_members = dir(function_set)
         filtered_members = [getattr(function_set, member_name) for member_name in raw_members
                             if member_name.startswith('check_')]
         self.methods = [member for member in filtered_members if inspect.ismethod(member)]
+
         self.function_set = function_set
+        self.messenger = messenger_obj
 
         if len(self.methods) == 0:
             raise ImproperlyConfigured(f'Your function_set object does not have any check methods.')
 
     def run_all_checks(self):
         for method in self.methods:
-            code, message, callback_data = method()
+            code, message_context, callback_data = method()
 
             if code == 0:  # means check was successful but did not find something useful
                 if method.__name__ not in self.function_set.silence:
-                    pass  # TODO: call a bot or email client
+                    try:
+                        self.messenger.send_completed_message(message_context)
+                    except NotImplementedError:
+                        self.messenger.send_message(message_context)
 
             if code == 1:  # means check was successful and found something it is meant to find
-                pass  # TODO: call a bot or email client
+                try:
+                    self.messenger.send_success_message(message_context)
+                except NotImplementedError:
+                    self.messenger.send_message(message_context)
 
             if code == -1:  # means there is an error during the check not on the developer side
-                pass  # TODO: call a bot or email client
+                try:
+                    self.messenger.send_errors_message(message_context)
+                except NotImplementedError:
+                    self.messenger.send_message(message_context)
 
             callback_obj = getattr(
                 self.function_set,
